efa1eaa67ab467092c4963e27094060ccca2a3a2
kudz2-0 <munashetawengwa26@gmail.com>
2023-02-28 04:17:20 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
 void print_alphabet(void)
 {
 int letter;
-for (letter = 'a'; letter <= 'z'; letter ++1)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-_puttchar(letter);
+_putchar(letter);
 }
 {
 _putchar (10);
