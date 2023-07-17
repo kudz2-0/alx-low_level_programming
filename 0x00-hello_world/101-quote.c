28714bf01b0407cc70e5bf73143352ab136a410a
root <kudz2-0@github.com>
2023-07-17 15:15:05 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Print some data
+ *Return: 1 alwaays (success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
