235fa9b5b44c44a4d73ab8a22d26719906908663
root <kudz2-0@github.com>
2023-07-18 07:01:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int n = 0;
+for (n = 0; n <= 9; n++)
+  {
+    printf("%d\n", n);
+  }
+putchar('\n');
+return (0);
+}
