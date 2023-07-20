36d7d87ce4ed05cd3f4c36f375682897eda15abd
root <kudz2-0@github.com>
2023-07-20 11:57:15 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Entry: always 0 (succeess)
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 97 ; i <= 122 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
